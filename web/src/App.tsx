b2293ca1a106260914baca6651a8e37462503cd9
Don Townsend <don@teamairship.com>
2020-06-26 15:41:52 -0400

    add typescript support; restructure src dir

---
+++ web/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import './App.scss';
+import SiteHeader from './_AppInternal/components/SiteHeader';
+
+function App() {
+  return (
+    <div className="App">
+      <SiteHeader />
+    </div>
+  );
+}
+
+export default App;
