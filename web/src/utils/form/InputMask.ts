f81b728a59fc5fd26e4932b08cc7d6df0e0befba
Don Townsend <don@teamairship.com>
2020-07-07 10:06:18 -0400

    add InputMask util

---
+++ web/src/utils/form/InputMask.ts
@@ -0,0 +1,455 @@
+
+/**
+ * INPUT MASK
+ *
+ * https://jsperf.com/inputmask-comparison
+ *
+ * Heavily modelled after imask.js; this aims to be a lean, minimalistic, functional, implementation-agnostic library
+ *
+ * **USAGE**:
+ *
+ * ```
+ * // phone number mask
+ * const inputMaskPhone = new InputMask({ mask: '[1 ](000) 000-0000' });
+ * const valFormatted = inputMaskPhone.mask('5409999999') // "(540) 999-9999";
+ * const valRaw = inputMaskPhone.unmask('(540) 999-9999') // "5409999999";
+ *
+ * // credit card number mask
+ * const inputMaskCC = new InputMask({ mask: '0000 0000 0000 0000' });
+ * const valFormatted = inputMaskCC.mask('1234123412341234') // "1234 1234 1234 1234";
+ * const valRaw = inputMaskCC.unmask('1234 1234 1234 1234') // "1234123412341234";
+ *
+ * // number mask
+ * const inputMaskNumber = new InputMask({ mask: Number });
+ * const valFormatted = inputMaskNumber.mask('12345.67') // "12,345.67";
+ * const valRaw = inputMaskNumber.unmask('12,345.67') // "12345.67";
+ *
+ * // date mask
+ * const inputMaskDate = new InputMask({ mask: Date });
+ * const valFormatted = inputMaskDate.mask('20201231') // "2020-12-31";
+ * const valRaw = inputMaskDate.unmask('2020-12-31') // "20201231";
+ *
+ * // alternate mask usage
+ * const inputMaskGeneric = new InputMask({ mask: "0000 0000" });
+ * inputMaskGeneric.process("12345678");
+ * inputMaskGeneric.value; // "1234 5678"
+ * inputMaskGeneric.unmaskedValue; // "12345678"
+ * ```
+ */
+
+const regexNonAlphaNumeric = /[|()[\]/\\\-+_.,\s]/gi;
+const regexNotNumeric = /[|()[\]/\\\-+_\sa-zA-Z]/gi;
+const regexDateDelimiter = /[|()[\]/\\\-+_.\sa-zA-Z]/gi;
+const regexNumeric = /[0-9]/i;
+const regexAlpha = /[a-zA-Z]/i;
+
+const CHAR_DIGIT = "0";
+const CHAR_ALPHA = "a";
+const CHAR_ANY = "*";
+const CHAR_ESCAPE = "\\";
+const DEFAULT_MASK = "";
+const DEFAULT_MASK_DELIMITER = "-";
+const DEFAULT_MASK_VALUE = "";
+const DEFAULT_DECIMAL_CHAR = ".";
+const DEFAULT_DECIMAL_PRECISION = Infinity;
+const DEFAULT_DATE_PATTERN = "YYYY-mm-dd";
+const DEFAULT_DATE_PATTERN_PARTS = ["yyyy", "mm", "dd"];
+const DEFAULT_PREFIX = "";
+const DEFAULT_PLACEHOLDER = "";
+
+const padWithZeros = (str, numFill = 0) => {
+  return (str || "")
+    .toString()
+    .padStart(numFill, "0");
+};
+
+type Mask = any;
+
+interface InputMaskConstructorOptions {
+  mask: Mask;
+  delimiter?: string;
+  maxLength?: number;
+  guide?: boolean;
+  decimalChar?: string;
+  datePattern?: string;
+  decimalPrecision?: number;
+  prefix?: string;
+}
+
+interface PatternPart {
+  value: string;
+  optional: boolean;
+  escape: boolean;
+}
+
+class InputMask {
+  _mask: Mask = DEFAULT_MASK;
+  _maskParts: any[] = [];
+  _maxLength: number = Infinity;
+  _guide: boolean = false;
+  _delimiter: string = DEFAULT_MASK_DELIMITER;
+  _delimiterRegex: RegExp;
+  _decimalChar: string = DEFAULT_DECIMAL_CHAR;
+  _decimalPrecision: number = DEFAULT_DECIMAL_PRECISION;
+  _datePattern: string = DEFAULT_DATE_PATTERN;
+  _datePatternParts: string[] = DEFAULT_DATE_PATTERN_PARTS;
+  _prefix: string = DEFAULT_PREFIX;
+  placeholder: string = DEFAULT_PLACEHOLDER;
+  value: string = DEFAULT_MASK_VALUE;
+  unmaskedValue: any = DEFAULT_MASK_VALUE;
+
+  constructor(
+    options: InputMaskConstructorOptions = {
+      mask: DEFAULT_MASK_VALUE,
+    }
+  ) {
+    this._checkOptions(options);
+    this._mask = options.mask || DEFAULT_MASK;
+    this._maxLength = options.maxLength || Infinity;
+    this._guide = options.guide || false;
+    this._delimiter = options.delimiter || this._getDefaultDelimiter();
+    this._delimiterRegex = new RegExp(`\\${this._delimiter}`, "g");
+    this._decimalChar = options.decimalChar || DEFAULT_DECIMAL_CHAR;
+    this._decimalPrecision = options.decimalPrecision || DEFAULT_DECIMAL_PRECISION;
+    this._datePattern = options.datePattern || DEFAULT_DATE_PATTERN;
+    this._prefix = options.prefix || DEFAULT_PREFIX;
+    this._maskParts = InputMask._preparePatternParts(options.mask);
+    this._datePatternParts = InputMask._prepareDatePatternParts(this._datePattern);
+    this.placeholder = this._getMaskPlaceholder();
+    return this;
+  }
+
+  _checkOptions(options: InputMaskConstructorOptions) {
+    if (!options.mask) {
+      throw new Error(
+        "constructor `options.mask` param required and cannot be empty"
+      );
+    }
+  }
+
+  static _preparePatternParts(mask: Mask): PatternPart[] {
+    if (!mask) return [];
+    if (typeof mask !== "string" && typeof mask !== "number") return [];
+    const _mask = (mask || "").toString();
+    let parts: PatternPart[] = [];
+    let buffer = "";
+    let optionalFlag = false;
+    let escapeNextChar = false;
+    const addPart = (value: any, optional = false, escape = false) => {
+      parts.push({
+        value,
+        optional,
+        escape,
+      });
+    };
+    for (let i = 0; i < _mask.length; i += 1) {
+      const char = _mask[i];
+      if (escapeNextChar) {
+        addPart(char, false, true);
+        escapeNextChar = false;
+        continue;
+      }
+      if (char === CHAR_ESCAPE) {
+        escapeNextChar = true;
+        continue;
+      }
+      if (char === "[") {
+        optionalFlag = true;
+        continue;
+      }
+      if (char === "]") {
+        addPart(buffer, true, false);
+        optionalFlag = false;
+        buffer = "";
+        continue;
+      }
+      if (optionalFlag) {
+        buffer += char;
+        continue;
+      }
+      addPart(char, false, false);
+    }
+    return parts;
+  }
+
+  static _prepareDatePatternParts(datePattern: string = ""): string[] {
+    return datePattern
+      .split(regexNonAlphaNumeric)
+      .filter(v => !!v)
+      .map(v => v.toLowerCase());
+  }
+
+  _getMaskPlaceholder() {
+    let dummyInput = "".padStart(this._mask.length, "9");
+    if (this._isTypeNumber()) {
+      dummyInput = "9999.99";
+    } else if (this._isTypeDate()) {
+      return this._datePatternParts.map(part => {
+        if (part.includes("y")) return "YYYY";
+        if (part.includes("m")) return "MM";
+        if (part.includes("d")) return "DD";
+        return "";
+      }).join(this._delimiter);
+    }
+    return this.mask(dummyInput) || DEFAULT_PLACEHOLDER;
+  }
+
+  _isTypeNumber() {
+    return false ||
+      this._mask === "Number" ||
+      this._mask === "number" ||
+      this._mask === Number ||
+      this._mask instanceof Number;
+  }
+
+  _isTypeDate() {
+    return false ||
+      this._mask === "Date" ||
+      this._mask === "date" ||
+      this._mask === Date ||
+      this._mask instanceof Date;
+  }
+
+  _getDefaultDelimiter() {
+    if (this._isTypeNumber()) {
+      return ',';
+    }
+    if (this._isTypeDate()) {
+      return '/';
+    }
+    return DEFAULT_MASK_DELIMITER;
+  }
+
+  _sanitizeInput(val): string {
+    const formatVal = (regexCharsToStrip: RegExp) => {
+      const _val = (!val && val !== 0) ? "" : val;
+      return _val.toString()
+        .replace(regexCharsToStrip, "")
+        .replace(this._delimiterRegex, "");
+    };
+    if (this._isTypeNumber()) {
+      return formatVal(regexNotNumeric);
+    }
+    if (this._isTypeDate()) {
+      return formatVal(regexDateDelimiter);
+    }
+    return (val || "").toString().replace(regexNonAlphaNumeric, "");
+  }
+
+  _round(num: string = "") {
+    if (num.length <= this._decimalPrecision || !this._decimalPrecision){
+      return num;
+    }
+    const rounded = ((parseInt(num, 10) || 0) / 10 ** num.length)
+      .toFixed(this._decimalPrecision);
+    if (rounded[0] === "1") return num.substring(0, this._decimalPrecision);
+    return rounded.substring(2);
+  }
+
+  _maskNumber(val) {
+    const _val: string = this._sanitizeInput(val);
+    if (!_val.length) return "";
+
+    const isLastCharDecimal = _val[_val.length - 1] === this._decimalChar;
+    const [numPartWhole, numPartDecimal] = _val.split(this._decimalChar).slice(0, 2);
+
+    if (!numPartWhole) {
+      if (isLastCharDecimal) {
+        return this._prefix + "0" + this._decimalChar;
+      }
+      return "";
+    };
+
+    let composed = "";
+    let placeValue = 0;
+    for (let i = numPartWhole.length - 1; i >= 0; i--) {
+      if (placeValue && !(placeValue % 3)) {
+        composed = this._delimiter + composed;
+      }
+      composed = numPartWhole[i] + composed;
+      placeValue++;
+    }
+    const suffix = numPartDecimal
+      ? `${this._decimalChar}${this._round(numPartDecimal)}`
+      : (isLastCharDecimal ? this._decimalChar : "");
+    const prefix = composed ? this._prefix : "";
+    return prefix + composed + suffix;
+  }
+
+  _maskDate(val) {
+    const _val: string = this._sanitizeInput(val);
+    const datePatternParts = this._datePatternParts;
+    const maxCursor = _val.length;
+    let composed = "";
+    let cursor = 0;
+    let currentDay: number = 0;
+    let currentMonth: number = 0;
+    let currentYear: number = 0;
+
+    const setTimeFromInput = (currentTimeUnit: "d" | "m" | "y", partial) => {
+      const intPartial = parseInt(partial, 10);
+      switch (currentTimeUnit) {
+        case "d":
+          currentDay = intPartial;
+          break;
+        case "m":
+          currentMonth = intPartial;
+          break;
+        case "y":
+          currentYear = intPartial;
+          break;
+        default:
+          break;
+      }
+    };
+
+    const captureChars = (currentTimeUnit: "d" | "m" | "y", numChars = 4, max = '9999') => {
+      if (cursor !== 0) {
+        composed += this._delimiter;
+      }
+      let partial = _val.substring(cursor, cursor + numChars);
+      if (partial.length === numChars) {
+        const isUnderMin = parseInt(partial, 10) === 0;
+        const isOverMax = parseInt(partial, 10) > parseInt(max, 10);
+        if (isUnderMin) {
+          partial = "01".padStart(numChars, "0");
+        } else if (isOverMax) {
+          partial = max;
+        }
+        setTimeFromInput(currentTimeUnit, partial);
+      } else if (this._guide) {
+        partial = partial.padEnd(numChars, "_");
+      }
+      composed += partial
+      cursor += partial.length;
+    };
+
+    for (let i = 0; i < datePatternParts.length; i++) {
+      const part = datePatternParts[i];
+      if (cursor >= maxCursor) {
+        if (!this._guide) break;
+        if (cursor !== 0) {
+          composed += this._delimiter;
+        }
+        if (part.includes('y')) {
+          composed += "____";
+          cursor += 4;
+        } else {
+          composed += "__";
+          cursor += 2;
+        }
+        continue;
+      }
+      if (part.includes('y')) {
+        captureChars('y', 4, '9999');
+        continue;
+      }
+      if (part.includes('m')) {
+        captureChars('m', 2, '12');
+        continue;
+      }
+      if (part.includes('d')) {
+        captureChars('d', 2, '31');
+        continue;
+      }
+    }
+
+    if (currentDay && currentMonth && currentYear) {
+      const d = new Date();
+      d.setFullYear(currentYear);
+      d.setMonth(currentMonth - 1);
+      d.setDate(currentDay);
+      const dateParts = [];
+      for (let i = 0; i < datePatternParts.length; i++) {
+        const part = datePatternParts[i];
+        if (part.includes('y')) {
+          dateParts.push(padWithZeros(d.getFullYear(), 4));
+          continue;
+        }
+        if (part.includes('m')) {
+          dateParts.push(padWithZeros(d.getMonth() + 1, 2));
+          continue;
+        }
+        if (part.includes('d')) {
+          dateParts.push(padWithZeros(d.getDate(), 2));
+          continue;
+        }
+      }
+      composed = dateParts.join(this._delimiter);
+    }
+    return composed;
+  }
+
+  mask(val) {
+    if (this._isTypeNumber()) return this._maskNumber(val);
+    if (this._isTypeDate()) return this._maskDate(val);
+
+    const _val: string = this._sanitizeInput(val);
+    const maxCursor = _val.length;
+    let composed = "";
+    let cursor = 0;
+    const pushChar = (char) => {
+      composed += char;
+      cursor++;
+    };
+    for (let i = 0; i < this._maskParts.length; i++) {
+      if (cursor >= this._maxLength) break;
+
+      const char = _val[cursor];
+      const part = this._maskParts[i];
+      const compareVal = part.value
+        .trim()
+        .toLowerCase()
+        .replace(regexNonAlphaNumeric, "");
+
+      if (part.escape) {
+        composed += part.value;
+        continue;
+      }
+
+      if (cursor >= maxCursor) {
+        if (!this._guide) break;
+        if (
+          compareVal === CHAR_DIGIT ||
+          compareVal === CHAR_ALPHA ||
+          compareVal === CHAR_ANY
+        ) {
+          pushChar("_");
+          continue;
+        }
+      }
+
+      if (
+        (compareVal === CHAR_DIGIT && regexNumeric.test(char)) ||
+        (compareVal === CHAR_ALPHA && regexAlpha.test(char)) ||
+        compareVal === CHAR_ANY
+      ) {
+        pushChar(char);
+        continue;
+      }
+
+      if (part.optional) {
+        if (compareVal === char) {
+          pushChar(part.value);
+        }
+        continue;
+      }
+
+      composed += part.value;
+    }
+    const prefix = composed ? this._prefix : "";
+    return prefix + composed;
+  }
+
+  unmask(val) {
+    return this._sanitizeInput(val);
+  }
+
+  resolve(val) {
+    this.value = this.mask(val);
+    this.unmaskedValue = this.unmask(val);
+    return this;
+  }
+}
+
+export default InputMask;
---
+++ web/src/utils/form/__TEST__/InputMask.test.js
@@ -0,0 +1,208 @@
+
+import InputMask from '../InputMask';
+
+test('input masks work correctly for phone numbers', () => {
+  const i1 = new InputMask({ mask: '[1 ](000) 000-0000' });
+  expect(i1.mask('19999999999')).toEqual('1 (999) 999-9999');
+
+  const i2 = new InputMask({ mask: '[1 ](000)-000-0000' });
+  expect(i2.mask('7775553333')).toEqual('(777)-555-3333');
+
+  // handle max length
+  const i3 = new InputMask({ mask: '[1 ](000)-000-0000', maxLength: 11 });
+  expect(i3.mask('7775553333124564654654')).toEqual('(777)-555-3333');
+
+  // handle extra chars
+  const i4 = new InputMask({ mask: '[1 ](000)-000-0000' });
+  expect(i4.mask('(777) 555-3333')).toEqual('(777)-555-3333');
+  expect(i4.mask('(777)-555-3333')).toEqual('(777)-555-3333');
+  expect(i4.mask('777 555-3333')).toEqual('(777)-555-3333');
+
+  // handle partials
+  const i5 = new InputMask({ mask: '[1 ](000) 000-0000' });
+  expect(i5.mask('777')).toEqual('(777');
+  expect(i5.mask('7775')).toEqual('(777) 5');
+  expect(i5.mask('777555')).toEqual('(777) 555');
+  expect(i5.mask('77755560')).toEqual('(777) 555-60');
+
+  // handle guide mode
+  const i6 = new InputMask({ mask: '[1 ](000) 000-0000', guide: true });
+  expect(i6.mask('777')).toEqual('(777) ___-____');
+  expect(i6.mask('7775')).toEqual('(777) 5__-____');
+  expect(i6.mask('777555')).toEqual('(777) 555-____');
+  expect(i6.mask('77755560')).toEqual('(777) 555-60__');
+  expect(i6.mask('777555601')).toEqual('(777) 555-601_');
+  expect(i6.mask('7775556010')).toEqual('(777) 555-6010');
+  expect(i6.mask('')).toEqual('(___) ___-____');
+
+  // handle escaped characters
+  const i7 = new InputMask({ mask: '1 (8\\0\\0) 000-0000' });
+  expect(i7.mask('1234567')).toEqual('1 (800) 123-4567');
+  const i8 = new InputMask({ mask: '\\0\\1\\2\\3 0000 0000 0000' });
+  expect(i8.mask('123412341234')).toEqual('0123 1234 1234 1234');
+
+  // handle prefix
+  const i9 = new InputMask({ mask: '0000 0000 0000 0000', prefix: "visa " });
+  expect(i9.mask('1111222233334444')).toEqual('visa 1111 2222 3333 4444');
+  expect(i9.mask('1111')).toEqual('visa 1111');
+  expect(i9.mask('')).toEqual('');
+});
+
+test('input mask resolve works as expected', () => {
+  const i1 = new InputMask({ mask: '[1 ](000) 000-0000' });
+  i1.resolve('1-999-999-9999');
+  expect(i1.value).toEqual('1 (999) 999-9999');
+  expect(i1.unmaskedValue).toEqual('19999999999');
+});
+
+test('input masks work correctly for credit cards', () => {
+  const inputMaskCC = new InputMask({ mask: '0000 0000 0000 0000', maxLength: 16 });
+
+  expect(inputMaskCC.mask('1234123412341234')).toEqual('1234 1234 1234 1234');
+  expect(inputMaskCC.mask('12341234')).toEqual('1234 1234');
+});
+
+test('input mask constructed with correct default delimeter', () => {
+  const i1 = new InputMask({ mask: Number });
+  expect(i1._delimiter).toEqual(',');
+  const i2 = new InputMask({ mask: Date });
+  expect(i2._delimiter).toEqual('/');
+  const i3 = new InputMask({ mask: Number, delimiter: '|' });
+  expect(i3._delimiter).toEqual('|');
+  const i4 = new InputMask({ mask: "0000 0000" });
+  expect(i4._delimiter).toEqual('-');
+});
+
+test('input mask works for number type', () => {
+  const i1 = new InputMask({ mask: Number });
+  expect(i1.mask()).toEqual('');
+  expect(i1.mask(0)).toEqual('0');
+  expect(i1.mask(1)).toEqual('1');
+  expect(i1.mask(15)).toEqual('15');
+  expect(i1.mask(150)).toEqual('150');
+  expect(i1.mask(1500)).toEqual('1,500');
+  expect(i1.mask(15000)).toEqual('15,000');
+  expect(i1.mask(15000.1)).toEqual('15,000.1');
+  expect(i1.mask(15000.11)).toEqual('15,000.11');
+  expect(i1.mask('.')).toEqual('0.');
+  expect(i1.mask('15000.')).toEqual('15,000.');
+  expect(i1.mask('15000.0')).toEqual('15,000.0');
+  expect(i1.mask('15000.00')).toEqual('15,000.00');
+  expect(i1.mask('15000000.00')).toEqual('15,000,000.00');
+
+  // handle user input
+  const iu = new InputMask({ mask: Number });
+  expect(iu.mask('1,1')).toEqual('11');
+  expect(iu.mask('11,11')).toEqual('1,111');
+  expect(iu.mask('111,11')).toEqual('11,111');
+  expect(iu.mask('111,111')).toEqual('111,111');
+  expect(iu.mask('1111,111')).toEqual('1,111,111');
+  expect(iu.mask('1111,1111')).toEqual('11,111,111');
+  expect(iu.mask('1,111,1111')).toEqual('11,111,111');
+  expect(iu.mask('1,111,11111')).toEqual('111,111,111');
+
+  // handle different number formats
+  const i2 = new InputMask({ mask: Number, delimiter: '.', decimalChar: ',' });
+  expect(i2.mask('15000,00')).toEqual('15.000,00');
+  expect(i2.mask('150000,00')).toEqual('150.000,00');
+  expect(i2.mask('1500000,00')).toEqual('1.500.000,00');
+
+  // handle currency prefix
+  const i3 = new InputMask({ mask: Number, prefix: '$' });
+  expect(i3.mask('')).toEqual('');
+  expect(i3.mask('1')).toEqual('$1');
+  expect(i3.mask('11')).toEqual('$11');
+  expect(i3.mask('111')).toEqual('$111');
+  expect(i3.mask('1111')).toEqual('$1,111');
+  expect(i3.mask('11111')).toEqual('$11,111');
+  expect(i3.mask('111111')).toEqual('$111,111');
+  expect(i3.mask('1111111')).toEqual('$1,111,111');
+  expect(i3.mask('15000000.00')).toEqual('$15,000,000.00');
+
+  // handle decimal precision
+  const i4 = new InputMask({ mask: Number, prefix: '$', decimalPrecision: 2 });
+  expect(i4.mask('')).toEqual('');
+  expect(i4.mask('.')).toEqual('$0.');
+  expect(i4.mask('0')).toEqual('$0');
+  expect(i4.mask('0.')).toEqual('$0.');
+  expect(i4.mask('0.0')).toEqual('$0.0');
+  expect(i4.mask('0.00')).toEqual('$0.00');
+  expect(i4.mask('0.000')).toEqual('$0.00');
+  expect(i4.mask('0.1')).toEqual('$0.1');
+  expect(i4.mask('0.11')).toEqual('$0.11');
+  expect(i4.mask('0.111')).toEqual('$0.11');
+  expect(i4.mask('0.114')).toEqual('$0.11');
+  expect(i4.mask('0.115')).toEqual('$0.12');
+  expect(i4.mask('0.119')).toEqual('$0.12');
+  expect(i4.mask('0.121')).toEqual('$0.12');
+  expect(i4.mask('0.125')).toEqual('$0.13');
+  expect(i4.mask('11.125')).toEqual('$11.13');
+  expect(i4.mask('1111111.999')).toEqual('$1,111,111.99');
+  expect(i4.mask('1111111.9999')).toEqual('$1,111,111.99');
+  expect(i4.mask(1111111.9999)).toEqual('$1,111,111.99');
+  const i5 = new InputMask({ mask: Number, decimalPrecision: 4 });
+  expect(i5.mask(3.14159265359)).toEqual('3.1416');
+
+  // handle different number format AND decimal precision
+  const i6 = new InputMask({ mask: Number, prefix: '£', delimiter: '.', decimalChar: ',', decimalPrecision: 2 });
+  expect(i6.mask('')).toEqual('');
+  expect(i6.mask('.')).toEqual('');
+  expect(i6.mask(',')).toEqual('£0,');
+  expect(i6.mask('12345')).toEqual('£12.345');
+  expect(i6.mask('12345,678')).toEqual('£12.345,68');
+});
+
+test('input mask works for date type', () => {
+  const i1 = new InputMask({ mask: Date, datePattern: 'YYYY-mm-dd', delimiter: '-' });
+  expect(i1.mask('')).toEqual('');
+  expect(i1.mask('2')).toEqual('2');
+  expect(i1.mask('20')).toEqual('20');
+  expect(i1.mask('200')).toEqual('200');
+  expect(i1.mask('2001')).toEqual('2001');
+  expect(i1.mask('20010')).toEqual('2001-0');
+  expect(i1.mask('200101')).toEqual('2001-01');
+  expect(i1.mask('2001010')).toEqual('2001-01-0');
+  expect(i1.mask('20010101')).toEqual('2001-01-01');
+  expect(i1.mask('20010131')).toEqual('2001-01-31');
+  expect(i1.mask('20011231')).toEqual('2001-12-31');
+  expect(i1.mask('20201231')).toEqual('2020-12-31');
+  expect(i1.mask('2020-12-31')).toEqual('2020-12-31');
+  expect(i1.mask('2020/12/31')).toEqual('2020-12-31');
+  expect(i1.mask('2020.12.31')).toEqual('2020-12-31');
+
+  // handle mm-yy format
+  const i2 = new InputMask({ mask: Date, datePattern: 'mm-yy' });
+  expect(i2.mask('021990')).toEqual('02/1990');
+  expect(i2.mask('02-1990')).toEqual('02/1990');
+
+  // handle mm-dd-yyyy format
+  const i3 = new InputMask({ mask: Date, datePattern: 'mm-dd-yyyy' });
+  expect(i3.mask('01012010')).toEqual('01/01/2010');
+  expect(i3.mask('12312020')).toEqual('12/31/2020');
+  expect(i3.mask('12-31-2020')).toEqual('12/31/2020');
+
+  // handle out-of-bound numbers
+  const i4 = new InputMask({ mask: Date, datePattern: 'mm-dd-yyyy' });
+  expect(i4.mask('00000000')).toEqual('01/01/0001');
+  expect(i4.mask('00330000')).toEqual('01/31/0001');
+  expect(i4.mask('99000000')).toEqual('12/01/0001');
+  expect(i4.mask('00009999')).toEqual('01/01/9999');
+
+  // handle date bleeding into next month (the logic here is based on Date.prototype.setDate internal functionality and doesn't reflect a particular use-case need)
+  expect(i4.mask('02292010')).toEqual('03/01/2010');
+  expect(i4.mask('02302010')).toEqual('03/02/2010');
+  expect(i4.mask('02312010')).toEqual('03/03/2010');
+
+  // handle date guide mode
+  const i5 = new InputMask({ mask: Date, guide: true });
+  expect(i5.mask('')).toEqual('____/__/__');
+  expect(i5.mask('2')).toEqual('2___/__/__');
+  expect(i5.mask('20')).toEqual('20__/__/__');
+  expect(i5.mask('200')).toEqual('200_/__/__');
+  expect(i5.mask('2000')).toEqual('2000/__/__');
+  expect(i5.mask('20001')).toEqual('2000/1_/__');
+  expect(i5.mask('200011')).toEqual('2000/11/__');
+  expect(i5.mask('2000111')).toEqual('2000/11/1_');
+  expect(i5.mask('20001111')).toEqual('2000/11/11');
+  expect(i5.mask('20200101')).toEqual('2020/01/01');
+});
